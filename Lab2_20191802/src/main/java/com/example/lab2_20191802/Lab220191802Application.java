87c7d5c41a0f093a15e08f3bdfddfb2c61f619af
Xavier <x.gavidia@pucp.edu.pe>
2024-09-04 20:10:12 -0500

    V1

---
+++ Lab2_20191802/src/main/java/com/example/lab2_20191802/Lab220191802Application.java
@@ -0,0 +1,13 @@
+package com.example.lab2_20191802;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab220191802Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab220191802Application.class, args);
+	}
+
+}
---
+++ Lab2_20191802/src/test/java/com/example/lab2_20191802/Lab220191802ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab2_20191802;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab220191802ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
